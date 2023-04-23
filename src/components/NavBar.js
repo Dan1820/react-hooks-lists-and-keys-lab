c8564ca77a119f0f4b3ee200afb1c805f40ebafd
Duncan Kipkemoi <kipkemoiduncan7@gmail.com>
2023-04-23 21:24:17 +0300

    completed

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,14 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
+  const refLinks= links.map((link)=>(
+    <a href={"#"+ link} key={link}>{link}</a>
+  ))
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>{/* display an <a> tag for each link here */}
+  {refLinks }
+
+  </nav>;
 }
 
 export default NavBar;
